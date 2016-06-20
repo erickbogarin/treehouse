5cf1026f25d8236e8e425324490fc1d5b46ebd37
Erick Bogarin <erick.bogarin@gmail.com>
2016-06-20 01:45:43 -0400

    Add compass into gulpfile instead sass task and increase Latest Project section structe/style

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp'),
-    plumber = require('gulp-plumber'),
-    rename = require('gulp-rename');
+plumber = require('gulp-plumber'),
+rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
+var compass = require('gulp-compass');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin'),
-    cache = require('gulp-cache');
+cache = require('gulp-cache');
 var minifycss = require('gulp-minify-css');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
@@ -13,7 +14,7 @@ var browserSync = require('browser-sync');
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
-       baseDir: "./"
+      baseDir: "./"
     }
   });
 });
@@ -24,43 +25,63 @@ gulp.task('bs-reload', function () {
 
 gulp.task('images', function(){
   gulp.src('src/img/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
-    .pipe(gulp.dest('dist/img/'));
+  .pipe(cache(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+  .pipe(gulp.dest('dist/img/'));
 });
 
 gulp.task('styles', function(){
   gulp.src(['src/css/style.scss'])
-    .pipe(plumber({
-      errorHandler: function (error) {
-        console.log(error.message);
-        this.emit('end');
+  .pipe(plumber({
+    errorHandler: function (error) {
+      console.log(error.message);
+      this.emit('end');
     }}))
-	.pipe(sass())
+    .pipe(sass())
     .pipe(autoprefixer('last 2 versions'))
     .pipe(gulp.dest('dist/css/'))
     .pipe(rename({suffix: '.min'}))
     .pipe(minifycss())
     .pipe(gulp.dest('dist/css/'))
     .pipe(browserSync.reload({stream:true}))
-});
+  });
 
-gulp.task('scripts', function(){
-  return gulp.src('src/js/**/*.js')
+  gulp.task('compass', function(){
+    gulp.src(['src/css/style.scss'])
     .pipe(plumber({
       errorHandler: function (error) {
         console.log(error.message);
         this.emit('end');
-    }}))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/js/'))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js/'))
-    .pipe(browserSync.reload({stream:true}))
-});
+      }}))
+      .pipe(compass({
+        css: 'src/css',
+        sass: 'src/css',
+        image: 'src/img'
+      }))
+      .pipe(autoprefixer('last 2 versions'))
+      .pipe(gulp.dest('dist/css/'))
+      .pipe(rename({suffix: '.min'}))
+      .pipe(minifycss())
+      .pipe(gulp.dest('dist/css/'))
+      .pipe(browserSync.reload({stream:true}))
+    });
 
-gulp.task('default', ['browser-sync'], function(){
-  gulp.watch("src/css/**/*.scss", ['styles']);
-  gulp.watch("src/js/**/*.js", ['scripts']);
-  gulp.watch("*.html", ['bs-reload']);
-});
+    gulp.task('scripts', function(){
+      return gulp.src('src/js/**/*.js')
+      .pipe(plumber({
+        errorHandler: function (error) {
+          console.log(error.message);
+          this.emit('end');
+        }}))
+        .pipe(concat('main.js'))
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(browserSync.reload({stream:true}))
+      });
+
+      gulp.task('default', ['browser-sync'], function(){
+        gulp.watch("src/css/**/*.scss", ['compass']);
+        gulp.watch("src/js/**/*.js", ['scripts']);
+        gulp.watch("*.html", ['bs-reload']);
+      });
