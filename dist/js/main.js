71d0c479606e8f92b3edf90987f55543bcef64ea
Erick Bogarin <erick.bogarin@gmail.com>
2016-09-07 17:47:53 -0400

    add parallax effect

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,14 @@
 /* header scroll */
 function scrollBanner() {
   var scrollPos;
-  var headerText = document.querySelector('.introdution');
+  var header = document.querySelector('.introdution');
+  var headerText = document.querySelector('.introdution-title');
   scrollPos = window.scrollY;
 
   if (scrollPos <= 600) {
-      headerText.style.transform =  "translateY(" + (-scrollPos/3) +"px" + ")";
+      header.style.transform =  "translateY(" + (-scrollPos/3) +"px" + ")";
+      header.style.opacity = 1 - (scrollPos/600);
+      headerText.style.transform = "translateX(" + (scrollPos/3) +"px" + ")";
       headerText.style.opacity = 1 - (scrollPos/600);
   }
 }
